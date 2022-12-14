dd52536a4eee598020cd6e78b6ec7c8f888301eb
+John <+>
2022-12-14 14:57:01 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+const submitData = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: "john",
+      email: "john@gmail.com",
+    }),
+  };
+  
+  fetch("http://localhost:3000/users", submitData)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  });
